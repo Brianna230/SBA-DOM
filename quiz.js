e05c9321ab61728393656f8babb056db0efe9de9
Brianna Lamarre <briannalamarre@Briannas-Laptop.local>
2025-05-21 22:58:20 -0400

    first commit

--- quiz.js
+++ quiz.js
@@ -1,1 +1,30 @@
+let title = document.createElement("h2");
+console.log(title);
+title.innerHTML="Animal Quiz"
+document.body.prepend(title);
+
 document.body.style.backgroundColor = "#FFF8E7"
+let QQuestOne = document.querySelector(".Question1")
+console.log(QQuestOne);
+
+QQuestOne.innerHTML = "1. Which animal make the sound Oink?"
+
+let QQuestTwo = document.querySelector(".Question2")
+console.log(QQuestTwo);
+
+QQuestTwo.innerHTML = "2.Which animal live underwater?"
+
+let QQuestThree = document.querySelector(".Question3")
+console.log(QQuestThree);
+
+QQuestThree.innerHTML = "3.Which animal is a herboivore?"
+let QQuestFour = document.querySelector(".Question4")
+console.log(QQuestFour)
+
+QQuestFour.innerHTML = "4. Which animal can change colors went it touch it surrounding?"
+
+let QQuestFive = document.querySelector(".Question5")
+console.log(QQuestFive)
+QQuestFive.innerHTML = "5.Which animal is said to be the most intelligent"
+
+console.log(QQuestFive);
